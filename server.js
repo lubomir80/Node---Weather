bcd6af90d67186d655f6fecd970210dcfee8d432
Lubomir80 <Kutsiyl@gmail.com>
2023-12-27 20:16:11 +0100

    request query latitude longitude

--- server.js
+++ server.js
@@ -10,15 +10,39 @@ const thirdPartBaseURL = "http://api.weatherbit.io/v2.0/current";
 
 app.get("/weather", async (req, res) => {
    try {
+      const {
+         latitude,
+         longitude
+      } = req.query
+
+      if (!latitude) res.status(400).json({ message: "latitude parameter is mandatory" })
+
+      if (!longitude) res.status(400).json({ message: "longitude parameter is mandatory" })
+
+
       const response = await got.get(`${thirdPartBaseURL}`, {
          searchParams: {
             key: thirdPartApiKey,
-            lat: "52.134717",
-            lon: "21.004241"
+            lat: latitude,
+            lon: longitude
          },
          responseType: "json"
       });
-      res.json({ response: response.body })
+      const [weatherData] = response.body.data;
+
+
+      const {
+         city_name,
+         weather: { description },
+         temp
+      } = weatherData
+
+      res.json({
+         city_name,
+         description,
+         temp
+      })
+
    } catch (error) {
       res.status(500).json({ message: error.message })
    }
@@ -29,7 +53,6 @@ app.listen(PORT, (err) => {
    if (err) console.error("Error at server launch");
    console.log(`${PORT} is working`);
 })
-
 
 
 
